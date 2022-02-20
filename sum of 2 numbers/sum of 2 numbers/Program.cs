4c814c23650af58bc2dc114f2420fb25efc78555
Satyam-sagar2630 <99909799+Satyam-sagar2630@users.noreply.github.com>
2022-02-20 23:53:23 +0530

    Add files via upload

---
+++ sum of 2 numbers/sum of 2 numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace app
+{
+    class Program
+    {
+        static void Main(string[]arg)
+        {
+            int a;
+            int b;
+            int sum;
+           // Console.WriteLine("Enter two numbers :");
+            Console.WriteLine("enter first number");
+            a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter second number");
+            b=Convert.ToInt32(Console.ReadLine());
+            sum = a + b;
+            Console.WriteLine("sum="+sum);
+
+          
+
+
+
+
+        }
+    }
+}
+ 
